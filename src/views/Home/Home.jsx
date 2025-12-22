b7b246bcabfeb65477d47bf35d4948cc526ca9eb
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-22 16:19:50 +0530

    Dot operator removed

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -12,15 +12,19 @@ function Home() {
       <h1>Student List</h1>
       <div className="home-container">
       {students.map((StudObj) => {
+         const { name, email, city, gender } = StudObj;
+
         return(
+
+
         <div className='student-card'>
-            <h2 className='student-name'>{StudObj.name}</h2>
+            <h2 className='student-name'>{name}</h2>
             <p className='student-info'>
-              <Mail className='student-info-icon'/>{StudObj.email}</p>
+              <Mail className='student-info-icon'/>{email}</p>
             <p className='student-info'>
-              <Building2 className='student-info-icon'/>{StudObj.city}</p>
+              <Building2 className='student-info-icon'/>{city}</p>
               <p className='student-info'>
-              <Building2 className='student-info-icon'/>{StudObj.gender}</p>
+              <Building2 className='student-info-icon'/>{gender}</p>
             
         </div>
 ) 
