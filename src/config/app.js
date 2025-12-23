0ef5f35fef100e25cc016566474b44fceba62fb5
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-23 15:56:10 +0530

    Primary color provided

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,9 @@
 const App_Name = "Student Portal";
 const App_Description = "A portal to manage student information.";
 
-export { App_Name, App_Description };
+const BG_Color = "#f0f0f0";
+const Primary_Color = "#4CAF50";
+const Secondary_Color = "#FF5722";
+const Light_Color = "#FFFFFF";
+
+export { App_Name, App_Description, BG_Color, Primary_Color, Secondary_Color, Light_Color };
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,13 +3,14 @@ import './Home.css'
 import StudCard from '../../components/Studcard/StudCard.jsx'
 import { students } from '../../config/student'
 import { App_Name, App_Description } from '../../config/app.js'
+import { Primary_Color } from '../../config/app.js'
 
 
 function Home() {
   
     return (
       <div>
-        <h1 className='App_Name'>{App_Name}</h1>
+        <h1 className='App_Name' style={{color:Primary_Color}}>{App_Name}</h1>
         <p className='App_Description'>{App_Description}</p>
 
         <div className='home-container'>
