ee6bdf4c60c1140edc7eedbdff54276811f1cc59
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-23 15:57:30 +0530

    Secondary color provided

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './Home.css'
 import StudCard from '../../components/Studcard/StudCard.jsx'
 import { students } from '../../config/student'
-import { App_Name, App_Description } from '../../config/app.js'
-import { Primary_Color } from '../../config/app.js'
+import { App_Name, App_Description, Secondary_Color, Primary_Color } from '../../config/app.js'
+
 
 
 function Home() {
@@ -11,7 +11,7 @@ function Home() {
     return (
       <div>
         <h1 className='App_Name' style={{color:Primary_Color}}>{App_Name}</h1>
-        <p className='App_Description'>{App_Description}</p>
+        <p className='App_Description'style={{color:Secondary_Color}}>{App_Description}</p>
 
         <div className='home-container'>
           {students.map((student)=>{
