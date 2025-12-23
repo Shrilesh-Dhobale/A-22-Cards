c46d7bb0664716a5371efdaba8953069c2f64dad
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-23 15:36:36 +0530

    app.js imported

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import './Home.css'
 import StudCard from '../../components/Studcard/StudCard.jsx'
 import { students } from '../../config/student'
+import { App_Name, App_Description } from '../../config/app.js'
 
 
 function Home() {
   
     return (
       <div>
-        <h1>Students</h1>
+        <h1 className='App_Name'>{App_Name}</h1>
+        <p className='App_Description'>{App_Description}</p>
 
         <div className='home-container'>
           {students.map((student)=>{
-            const {name, email, city, gender, avatar} = student;
+            const {name, email, city, gender} = student;
             return (<StudCard
               key={email}
               name={name}
